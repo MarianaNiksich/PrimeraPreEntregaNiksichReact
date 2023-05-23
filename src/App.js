6606bd3ef31d6547aa83969b7e857fc5e00dd759
MarianaNiksich <niksichmariana01@gmail.com>
2023-05-23 17:35:45 -0300

    agregando props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './components/styles.css'
 import './App.css';
 import Navbar from './components/NavBar';
+import Greeter from './components/ItemListContainer';
+
 function App() {
   return (
     <div className="App">
       <Navbar />
-      funciona
+      <Greeter titulo="¡Bienvenidos a Book Storage!" parrafo="¡Descubre un mundo de conocimiento en nuestra librería online! Encuentra los mejores libros en todas las categorías imaginables y sumérgete en historias cautivadoras, aprendizaje fascinante y descubrimientos asombrosos." />
     </div>
   );
 }
